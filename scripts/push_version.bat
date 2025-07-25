baff5e9591c2f3988ce57aaea8f992cf1c9471b8
Jaap van der Velde <jaap.vandervelde@tuflow.bmt.org>
2025-07-25 13:21:31 +1000

    add explicit version check

--- scripts/push_version.bat
+++ scripts/push_version.bat
@@ -1,10 +1,13 @@
 @echo off
 REM change working directory to project root
 cd %~dp0\..
-call scripts/run_tests.bat
-if errorlevel 1 goto tests_failed
+set PYTHONPATH=%~dp0\..
+python scripts/check_version.py %1
+if errorlevel 1 goto version_issue
 git diff-index --quiet HEAD
 if errorlevel 1 goto uncommitted_changes
+call scripts/run_tests.bat
+if errorlevel 1 goto tests_failed
 git push origin
 git push github
 git tag %1
@@ -17,5 +20,8 @@ echo Some tests failed, please ensure no tests fail before deploying.
 
 :uncommitted_changes
 echo There are unstaged or uncommitted_changes.
+
+:version_issue
+echo Version issues need to be resolved first.
 
 :end
